d12ff1fe270933d7837d64c12070b6b7baf298f8
abdulhadi-labanie <abdulhadi.labanie1@gmail.com>
2026-08-24 10:17:44 +0300

    Using a def and make clean code :)

--- mod01/hello.py
+++
@@ -1,9 +0,0 @@
-
-print("Hello world!\n")
-
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-
-full_name = first_name + " " + last_name
-
-print("Hello : " + full_name)
