ddd2a6018a7fc478a1a285b5176ef857360cf64d
abdulhadi-labanie <abdulhadi.labanie1@gmail.com>
2026-08-25 13:52:29 +0300

    adding a 3.3 and 3.4 tasks

--- mod01-03/mod01-03-teht.py
+++ mod01-03/mod01-03-teht.py
@@ -6,8 +6,21 @@ def read_str_input(string):
     return input(string)
 
 
+def read_int_input(string):
+    return int(input(string))
+
+
 def read_float_input(string):
     return float(input(string))
+
+
+def int_range_loops(end):
+    nums = []
+    for i in range(1, end +1):
+        number = read_int_input(f"{i}. Enter a number: ")
+        nums.append(number)
+
+    return nums
 
 
 def get_full_name():
@@ -21,6 +34,38 @@ def calculating_area_of_circle(radius):
     return f"{(math.pi * radius**2):.2f}"
 
 
+def calculate_area_of_rectangle(rectangle_width, rectangle_length):
+    return rectangle_width * rectangle_length
+
+
+def sum_number(nums):
+    total = 0
+
+    for num in nums:
+        total += num
+
+    return total
+
+
+def product_number(nums):
+    total = 1
+
+    for num in nums:
+        total *= num
+
+    return total
+
+
+def average_of_numbers(nums):
+    total = 0
+    i = 0
+    for num in nums:
+        total += num
+        i += 1
+
+    return total / i
+
+
 def start():
     print("\n\n############## Tehtävä 3: ##############\n\n")
 
@@ -31,10 +76,15 @@ def start():
     print(f"Ympyrän pinta alan on = {calculating_area_of_circle(radius)}")
 
     print("\n\n3.3-")
-
+    rectangle_width = read_float_input("Enter a rectangle width: ")
+    rectangle_length = read_float_input("Enter a rectang lelength: ")
+    print(f"The area of rectangle is : {calculate_area_of_rectangle(rectangle_width,rectangle_length)}")
 
     print("\n\n3.4-")
-
+    number3_4 = int_range_loops(3)
+    print(f"\n- The sum of numbers is = {sum_number(number3_4)}")
+    print(f"\n- The product of numbers is = {product_number(number3_4)}")
+    print(f"\n- The average of numbers is = {average_of_numbers(number3_4)}")
 
     print("\n\n3.5-")
 
