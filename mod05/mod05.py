580bd6f472dbc5e3897b1a61b8acf238514454f6
abdulhadi-labanie <abdulhadi.labanie1@gmail.com>
2026-08-31 10:33:56 +0300

    adding task 5.3

--- mod05/mod05.py
+++ mod05/mod05.py
@@ -41,13 +41,29 @@ def print_nums_divided_into_three(nums_list):
         print(num, end=" ")
 
 
+def read_and_print_user_inch_values():
+    inch = 2.54
+
+    while True:
+        user_input = read_int_input(
+            "Enter an inchs to switch it to cm: ")
+
+        if user_input < 0:
+            break
+
+        else:
+            total = user_input * inch
+            print(f"The inchs {user_input} = {total}cm.")
+
+
+
 def read_user_nums(exception):
     smaller = None
     larger = None
 
     while True:
         user_input = read_int_input_with_exception(
-            "Enter a number (or \"enter\" to finish): ",
+            "Enter a number (or \"Enter\" to finish and get smaller and larger numbers): ",
             exception
         )
 
@@ -72,12 +88,21 @@ def start():
 
 
     print(f"\n5.2 -\n\n")
+    read_and_print_user_inch_values()
+
+
+    print(f"\n5.3 -\n\n")
     smaller, larger = read_user_nums("")
     print(f"\n- The smaller number is = {smaller}.")
     print(f"- The larger number is = {larger}.")
 
+    print(f"\n5.4 -\n\n")
 
 
+    print(f"\n5.5 -\n\n")
+
+
+    print(f"\n5.6 -\n\n")
 
 
 
