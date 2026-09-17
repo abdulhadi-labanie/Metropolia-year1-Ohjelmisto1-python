17e8b001fa100dd016850ca68e69d1ec5fe2f51f
abdulhadi-labanie <abdulhadi.labanie1@gmail.com>
2026-09-17 14:27:54 +0300

    adding a task 6.2 and read int input whith exception

--- mod06/mod06.py
+++ mod06/mod06.py
@@ -4,6 +4,21 @@ def read_str_input(string):
     return input(string)
 
 
+def read_int_input_with_exception(string, min_val, max_val, exception):
+    while True:
+        try:
+            user_input = input(string)
+            if user_input == exception:
+                return exception
+            user_input = int(user_input)
+            if user_input < min_val or user_input > max_val:
+                print(f"\t\tPlease choose a number between {min_val} and {max_val}!!!!!")
+                continue
+            return user_input  
+        except ValueError:
+            print("Please enter an integer!!!")
+
+
 def read_int_input(string, min_val, max_val):
     while True:
         try:
@@ -30,6 +45,26 @@ def sum_total_of_dice(dice_cont):
     return sum_total
 
 
+def get_number_form_user():
+    user_input_numbers = []
+
+    i = 0
+    while True:
+        print(i+1, end="")
+        user_input_num = read_int_input_with_exception(".Plase Enter a number: ",-1000000,1000000,"")
+
+        if user_input_num == "":
+            break
+        else:
+            user_input_numbers.append(user_input_num)
+            i += 1
+
+    return user_input_numbers, i
+
+
+def get_largest_numbers_in_list(user_list: list, nums=5):
+    user_list.sort(reverse=True)
+    return user_list[:nums]
 
 
 def start():
@@ -41,10 +76,12 @@ def start():
 
 
     print(f"\n6.2 -\n\n")
-
+    user_input_list_numbers , count = get_number_form_user()
+    largest_numbers_in_list = get_largest_numbers_in_list(user_input_list_numbers,5)
+    print(f"\nThe number you enter count are {count} and the largest 5 numbers you enter are: {largest_numbers_in_list}")
 
     print(f"\n6.3 -\n\n")
-
+    
 
     print(f"\n6.4 -\n\n")
 
